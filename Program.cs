c89de998d5e88bee68122d3cd680e4eac49cc04b
rafael-sander <rafael.sander@gmail.com>
2022-02-17 16:37:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            Console.WriteLine($"O número é: {i}");
+        }
+    }
+}
