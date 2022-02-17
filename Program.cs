43436e15386570996a261e8362d8f8fd6ec218ef
rafael-sander <rafael.sander@gmail.com>
2022-02-17 17:36:50 -0300

    Alteração numeroDeVezes de 10 para 5

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            int numeroDeVezes = 10;
+            int numeroDeVezes = 5;
 
             for (int i=0; i < numeroDeVezes; i++)
             Console.WriteLine($"O número é: {i}");
